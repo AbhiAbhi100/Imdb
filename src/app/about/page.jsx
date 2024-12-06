ccab9385e2e49b23f682bbcff725e076104efc14
AbhiAbhi100 <abhinavr99p@gmail.com>
2024-12-06 12:17:52 +0530

    about page add

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+
+      <p>
+      Welcome to IMDb Clone, a platform dedicated to providing movie and TV show enthusiasts 
+          with detailed information about their favorite titles. Whether you're exploring trending shows 
+          or learning more about iconic movies, IMDb Clone is your trusted guide to the world of entertainment.
+
+      </p>
+    </div>
+  )
+}
+
+export default page
