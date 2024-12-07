83858d52e50613fb719b00008c66c2c07b064fdc
AbhiAbhi100 <abhinavr99p@gmail.com>
2024-12-07 14:12:17 +0530

    loader effect added

---
+++ src/app/loading.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+export default function loading(){
+    return (
+        <div className='flex justify-center mt-16'>
+      <img className='h-52' src='Spinner.svg' alt='loading...' />
+    </div>
+
+    );
+}
